55ca238a41ff7cc30844e99e2150aa018496d619
kapil-huma <kapil@huma.ai>
2022-03-25 11:51:51 +0530

    fjf

---
+++ brews.rb
@@ -0,0 +1,14 @@
+
+class Brews < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/kapil-huma/brews/archive/refs/tags/v1.tar.gz"
+  sha256 "34b974a23aae60dd4957ab81cfc28d8a0ae13fd967a1b3f07ed74c986f5d65c4"
+  license ""
+
+
+  def install
+   	bin.install "brews" 
+  end
+
+end
